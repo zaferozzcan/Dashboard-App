2f759150364b958e1f4f8586eeffaf29609a9e27
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-06-17 16:11:05 -0400

    Add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
-import Dashboard from '../src/components/dashboard/Dashboardnpm '
+import Dashboard from '../src/components/dashboard/Dashboard'
 import ProjectDetails from "./components/projects/ProjectDetails"
 
 class App extends Component {
