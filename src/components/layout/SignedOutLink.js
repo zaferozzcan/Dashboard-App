5f80aeb9de336f50858507411fa0c5f4f2eadc40
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-06-27 14:42:17 -0400

    Add modified Signed in and Signed out links

--- src/components/layout/SignedOutLink.js
+++ src/components/layout/SignedOutLink.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { NavLink } from 'react-router-dom'
 
-function SignedOutLink(){
-    return (
-        <div>
-        <ul className="right">
-          <li><NavLink to='/'>Signup</NavLink></li>
-          <li><NavLink to='/'>Login</NavLink></li>
-        </ul>
-      </div>
-    )
+function SignedOutLink() {
+  return (
+    <div>
+      <ul className="right">
+        <li><NavLink to='/signup'>Signup</NavLink></li>
+        <li><NavLink to='/signin'>Login</NavLink></li>
+      </ul>
+    </div>
+  )
 }
 
 export default SignedOutLink;
