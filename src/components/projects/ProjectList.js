9127630b1a8f13d5ec59c3ac21f945663d11ca87
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-06-28 14:44:34 -0400

    Add dummy project to test out passing projects on the dashboard

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,20 +1,16 @@
 import React from 'react'
-import ProjectSummary from "./ProjectSummary"
+import ProjectSummary from './ProjectSummary'
 
-
-const ProjectList = () => {
+const ProjectList = ({ projects }) => {
     return (
         <div className="project-list section">
-
-            <ProjectSummary/>
-
-            <ProjectSummary/>
-
-            <ProjectSummary/>
-
-
+            {projects && projects.map(project => {
+                return (
+                    <ProjectSummary project={project} key={project.id} />
+                )
+            })}
         </div>
     )
 }
 
-export default ProjectList;
+export default ProjectList
