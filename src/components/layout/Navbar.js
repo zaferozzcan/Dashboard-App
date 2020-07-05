eb80727abcee82dea64cf03623e29dfd9c01a903
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-07-05 14:57:58 -0400

    Add FirefooFirebase Auth with Redux

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,20 +1,27 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import SignedInLink from "./SignedInLink";
-import SignedOutLink from "./SignedOutLink";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import SignedInLinks from './SignedInLinks'
+import SignedOutLinks from './SignedOutLinks'
+import { connect } from 'react-redux'
 
-
-
-function Navbar(){
+const Navbar = (props) => {
+    const { auth } = props;
     return (
-       <nav className="nav-wrapper.grey.darken-3">
-           <div className="container">   
-                <Link to="/" className = "brand-logo"> Zafer Ozcan </Link>
-                <SignedInLink />
-                <SignedOutLink />
-           </div>
-       </nav>
+        <nav className="nav-wrapper grey darken-3">
+            <div className="container">
+                <Link to='/' className="brand-logo">MarioPlan</Link>
+                <SignedInLinks />
+                <SignedOutLinks />
+            </div>
+        </nav>
     )
 }
 
-export default Navbar;
+const mapStateToProps = (state) => {
+    // console.log(state);
+    return {
+        auth: state.firebase.auth
+    }
+}
+
+export default connect(mapStateToProps)(Navbar)
