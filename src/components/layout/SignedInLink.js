3a424511894f6467c6330092ec1d343c4660ac76
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-07-08 14:57:06 -0400

    Add auth reducer and sign in links

--- src/components/layout/SignedInLink.js
+++ src/components/layout/SignedInLink.js
@@ -1,15 +1,25 @@
-import React from "react";
+
+import React from 'react'
 import { NavLink } from 'react-router-dom'
-function SignedInLink() {
+import { connect } from 'react-redux'
+import { signOut } from '../../store/actions/authActions'
+
+const SignedInLinks = (props) => {
     return (
         <div>
             <ul className="right">
                 <li><NavLink to='/create'>New Project</NavLink></li>
-                <li><NavLink to='/'>Log Out</NavLink></li>
+                <li><a onClick={props.signOut}>Log Out</a></li>
                 <li><NavLink to='/' className="btn btn-floating pink lighten-1">NN</NavLink></li>
             </ul>
         </div>
     )
 }
 
-export default SignedInLink;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        signOut: () => dispatch(signOut())
+    }
+}
+
+export default connect(null, mapDispatchToProps)(SignedInLinks)
